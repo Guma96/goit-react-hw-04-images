9d0b115c2b06aa0e4884a0ff531aed56bf139643
Guma96 <mateusz.kutarba96@gmail.com>
2023-06-30 12:15:31 +0200

    Update fetchImages.jsx

--- src/fetchImages/fetchImages.jsx
+++ src/fetchImages/fetchImages.jsx
@@ -5,7 +5,7 @@ axios.defaults.baseURL = `https://pixabay.com/api`;
 export const getGallery = async (query, page) => {
   try {
     const response = await axios.get(
-      `/?q=${query}&page=${page}&key=36957207-c1d21013f0aa2c58ca97c354a&image_type=photo&orientation=horizontal&per_page=12`
+      `/?q=${query}&page=${page}&key=34859493-53a9b5cb834f8dfe1be273cf4&image_type=photo&orientation=horizontal&per_page=12`
     );
 
     return response.data;
